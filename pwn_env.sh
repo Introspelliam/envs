89e3517ef6f10802d77e14c0b3156a09362f451d
Introspelliam <Inspelliam@163.com>
2019-05-11 13:03:55 +0800

    Update pwn_env.sh

--- pwn_env.sh
+++ pwn_env.sh
@@ -9,7 +9,7 @@ sudo apt-get -y install gcc-multilib g++-multilib
 sudo apt-get -y install libc6-dev-i386
 sudo apt-get -y install git
 sudo apt-get -y install libc6-dbg libc6-dbg:i386
-sudo apt-get -y install nmap
+sudo apt-get -y install nmap libseccomp-dev
 sudo apt-get -y install python-pip libssl-dev
 sudo apt-get -y install gdb
 sudo pip install --upgrade pip
