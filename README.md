5f919a15f02d0f79fc29029619d1aa5606aae1dc
inspelliam <inspelliam@163.com>
2020-11-15 10:33:44 +0800

    update tmux and vimrc

--- README.md
+++ README.md
@@ -6,3 +6,27 @@
 git clone https://github.com/Introspelliam/envs.git
 cd envs && chmod u+x pwn_env.sh && ./pwn_env.sh
 ```
+
+## python
+
+```
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r py2-requirements.txt --user
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple -r py3-requirements.txt --user
+```
+
+## vim
+
+```
+vim +PluginInstall +qall    # command line install
+:PluginInstall              # install in any vim window
+```
+
+## mux
+
+```
+tmux
+tmux source ~/.tmux.conf
+prefix+I           # install plugins, I is upper
+prefix+U           # upgrade plugins, U is upper
+```
+
--- pwn_env.sh
+++ pwn_env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
+cp .tmux.conf ~/.tmux.conf
 cd $HOME
 sudo dpkg --add-architecture i386
 sudo apt-get -y update
@@ -7,29 +8,37 @@ sudo apt-get -y upgrade
 sudo apt-get -y install binutils nasm
 sudo apt-get -y install gcc-multilib g++-multilib
 sudo apt-get -y install libc6-dev-i386
-sudo apt-get -y install git
+sudo apt-get -y install git vim tmux cscope ctags
 sudo apt-get -y install libc6-dbg libc6-dbg:i386
 sudo apt-get -y install nmap libseccomp-dev
 sudo apt-get -y install python-pip libssl-dev
 sudo apt-get -y install gdb
 sudo apt-get -y install nodejs-dev node-gyp libssl1.0-dev
-sudo apt-get -y install npm
+sudo apt-get -y install npm libmpc-dev openssh-server 
+sudo ufw allow 22
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source-file ~/.tmux.conf
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 sudo pip install --upgrade pip
 sudo pip install --upgrade capstone
 sudo pip install --upgrade pwntools
-sudo pip install ropgadget
+sudo pip install ropgadget one_gadget
 
 git clone https://github.com/scwuaptx/peda.git ~/peda 
 cp ~/peda/.inpurc ~/
 git clone https://github.com/scwuaptx/Pwngdb.git
 cp ~/Pwngdb/.gdbinit ~/
 git clone https://github.com/JonathanSalwan/ROPgadget
-
 git clone https://github.com/cloudburst/libheap.git
 sudo pip3 install --user ./libheap/
 echo "python from libheap import *" >> ~/.gdbinit
 echo "set follow-fork-mode child" >> ~/.gdbinit
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+./setup.sh
+cd $HOME
 
 sudo apt-get -y install socat
 sudo apt-get -y install xinetd
