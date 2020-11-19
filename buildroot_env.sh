5f68b8f3f1ade18c27b9fec394437cdaee112aad
Introspelliam <inspelliam@163.com>
2020-11-19 10:50:54 -0800

    update pwn_env and py3

---
+++ buildroot_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install -y debconf libncurses-dev
+# Install the libncurses5 and kpartx packages.
+sudo apt-get install -y libncurses5 libncurses5-dev kpartx
---
+++ qemu_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt install -y iasl git-lfs libcap-ng-dev libpcap-dev device-tree-compiler libglib2.0-dev libpixman-1-dev
+sudo apt install -y uuid-dev libbz2-dev libpulse0 libsdl2-dev libgtk-3-0 libgtk-3-dev libsnappy-dev libspice-server-dev
